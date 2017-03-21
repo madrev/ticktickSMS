4f3c00abfa0c35e63e2bf22c564cfec41f5d96f2
Maddie Revill <maddierevill@gmail.com>
2017-03-21 12:53:28 -0700

    Use threading.Timer instead of scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,14 +1,10 @@
 import ticktick
-import sched, time
 import threading
 
-s = sched.scheduler(time.time, time.sleep)
+def schedule_message(min, phone, message):
+    timer = threading.Timer(min*60, ticktick.send_sms, (phone, message))
+    timer.start()
+    return timer
 
-def schedule_message(sec, phone, message):
-    event = s.enter(sec, 1, ticktick.send_sms, (phone, message))
-    run_scheduler(s)
-    return event
-
-def run_scheduler(s):
-    if len(s.queue) == 1:
-        threading.Thread(target=s.run).start()
+def cancel_message(timer):
+    timer.cancel()
