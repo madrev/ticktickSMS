2fce19acbac354bccf5732b7b13392def0322f27
Maddie Revill <maddierevill@gmail.com>
2017-03-21 11:12:46 -0700

    Look up and direct messages to recipient's phone number

--- ticktick.py
+++ ticktick.py
@@ -5,13 +5,11 @@ from twilio import twiml
 from twilio.rest import TwilioRestClient
 
 
-SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_INCOMING_TOKEN', None)
-TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER', None)
-USER_NUMBER = os.environ.get('USER_NUMBER', None)
 
 app = Flask(__name__)
-slack_client = SlackClient(os.environ.get('SLACK_TOKEN', None))
 twilio_client = TwilioRestClient()
+slack_client = SlackClient(os.environ.get('SLACK_TOKEN', None))
+SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_INCOMING_TOKEN', None)
 TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER', None)
 USER_NUMBER = os.environ.get('USER_NUMBER', None)
 
@@ -24,16 +22,19 @@ def get_recipient_id(channel_id):
                 return im['user']
     return None
 
-
+def get_user_phone(user_id):
+    user_call = slack_client.api_call("users.info", user=user_id)
+    if user_call.get('ok') and 'phone' in user_call['user']['profile']:
+        return user_call['user']['profile']['phone']
+    return None
 
 @app.route('/twilio', methods=['POST'])
 def twilio_post():
     response = twiml.Response()
-    if request.form['From'] == USER_NUMBER:
-        message = request.form['Body']
-        slack_client.api_call("chat.postMessage", channel="#ticktick",
-                              text=message, username='ticktick',
-                              icon_emoji=':robot_face:', reply_broadcast="true")
+    message = request.form['Body']
+    slack_client.api_call("chat.postMessage", channel="#ticktick",
+                          text=message, username='ticktick',
+                          icon_emoji=':robot_face:', reply_broadcast="true")
     return Response(response.toxml(), mimetype="text/xml"), 200
 
 @app.route('/slack', methods=['POST'])
@@ -41,13 +42,16 @@ def slack_post():
     if request.form['token'] == SLACK_WEBHOOK_SECRET:
         print(request.form)
         channel_id = request.form['channel_id']
-        print(find_recipient_id(channel_id))
-        channel = request.form['channel_name']
+        recipient_phone = get_user_phone(get_recipient_id(channel_id))
         username = request.form['user_name']
         text = request.form['text']
-        response_message = username + " in " + channel + " says: " + text
-        twilio_client.messages.create(to=USER_NUMBER, from_=TWILIO_NUMBER,
-                                      body=response_message)
+        response_message = username + " says: " + text
+        if recipient_phone != None:
+            twilio_client.messages.create(to=recipient_phone, from_=TWILIO_NUMBER,
+                                          body=response_message)
+        else:
+            # TODO: tell the user the message isn't happening
+            print("No text sent")
     return Response(), 200
 
 
