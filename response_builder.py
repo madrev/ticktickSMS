f6ff6b3ebc8db778f40532e1a7c3411c8dcf392e
Maddie Revill <maddierevill@gmail.com>
2017-03-22 09:53:46 -0700

    Move response builders into separate file

---
+++ response_builder.py
@@ -0,0 +1,39 @@
+import json
+
+def build_success_response(time, message, timer_id):
+    resp = {
+        "response_type": "in_channel",
+        "text": f"Your message will be sent in {time} minutes.",
+        "attachments": [
+        {
+        "text": message
+        },
+        {
+        "callback_id": timer_id,
+        "fallback": "Sorry, your browser doesn't support canceling this action.",
+        "actions": [
+            {
+            "name":"cancel",
+            "text":"Cancel this SMS",
+            "style":"danger",
+            "type":"button"
+            }
+        ]
+            }
+        ]
+    }
+    return json.dumps(resp)
+
+def build_failure_response():
+    resp = {
+    "response_type": "ephemeral",
+    "text": "Sorry, your recipient does not have a phone number listed."
+    }
+    return json.dumps(resp)
+
+def build_cancel_response():
+    resp = {
+    "replace_original": True,
+    "text": "Your message has been canceled."
+    }
+    return json.dumps(resp)
--- ticktick.py
+++ ticktick.py
@@ -2,6 +2,8 @@ import os
 import scheduler
 import json
 import pprint
+import redis
+from response_builder import build_success_response, build_failure_response, build_cancel_response
 from flask import Flask, request, Response
 from slackclient import SlackClient
 from twilio import twiml
@@ -10,11 +12,12 @@ from twilio.rest import TwilioRestClient
 
 
 app = Flask(__name__)
+db = redis.StrictRedis(host='localhost', port=6379, db=0)
+
 twilio_client = TwilioRestClient()
 slack_client = SlackClient(os.environ.get('SLACK_TOKEN', None))
-SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_INCOMING_TOKEN', None)
+SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET', None)
 TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER', None)
-USER_NUMBER = os.environ.get('USER_NUMBER', None)
 
 timers = {
 "counter": 0
@@ -44,43 +47,9 @@ def send_sms(phone, message):
     twilio_client.messages.create(to=phone, from_=TWILIO_NUMBER,
                                   body=message)
 
-def build_success_response(time, message, timer_id):
-    resp = {
-        "response_type": "in_channel",
-        "text": f"Your message will be sent in {time} minutes.",
-        "attachments": [
-        {
-        "text": message
-        },
-        {
-        "callback_id": timer_id,
-        "fallback": "Sorry, your browser doesn't support canceling this action.",
-        "actions": [
-            {
-            "name":"cancel",
-            "text":"Cancel this SMS",
-            "style":"danger",
-            "type":"button"
-            }
-        ]
-            }
-        ]
-    }
-    return json.dumps(resp)
 
-def build_failure_response():
-    resp = {
-    "response_type": "ephemeral",
-    "text": "Sorry, your recipient does not have a phone number listed."
-    }
-    return json.dumps(resp)
 
-def build_cancel_response():
-    resp = {
-    "replace_original": True,
-    "text": "Your message has been canceled."
-    }
-    return json.dumps(resp)
+
 
 @app.route('/twilio', methods=['POST'])
 def twilio_post():
@@ -95,7 +64,8 @@ def twilio_post():
 def slack_post():
     if request.form['token'] == SLACK_WEBHOOK_SECRET:
         channel_id = request.form['channel_id']
-        recipient_phone = get_user_phone(get_recipient_id(channel_id))
+        recipient_id = get_recipient_id(channel_id)
+        recipient_phone = get_user_phone(recipient_id)
         username = request.form['user_name']
         text = request.form['text']
         response_message = username + " says: " + text
